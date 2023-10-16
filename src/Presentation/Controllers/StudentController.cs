da05d7f24fc0f2e9cfd41414969bab4c273d9863
Duong Thang <duongthang0702@gmail.com>
2023-10-16 22:16:36 +0700

    Get Student, fix error change role for user

---
+++ src/Presentation/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Services.Student;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudent()
+        {
+            var response = await _studentService.GetAllStudent();
+            if (response == null) return NotFound("not found students");
+            return Ok(response);
+        }
+    }
+}
